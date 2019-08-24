175247d92dd68ea591dcaa8c7a930da7a9267216
Ken Eguchi <no.eggchicken@gmail.com>
2019-08-25 08:00:03 +0900

    Add custom theme

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,37 @@
 import React from 'react'
 import Page from './page';
 
+import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import primary from '@material-ui/core/colors/yellow';
+import secondary from '@material-ui/core/colors/orange';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      "PixelMplus10"
+    ]
+  },
+  palette: {
+    primary: primary,
+    secondary: secondary
+  },
+  overrides: {
+    MuiButton: {
+      text: {
+        color: "white"
+      },
+      outlined: {
+        borderRadius: 0
+      }
+    }
+  }
+})
+
 export default () => {
   return (
-    <Page />
+    <ThemeProvider theme={theme}>
+      <Page />
+    </ThemeProvider>
   )
 }
