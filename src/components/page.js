370e41981b4410c771d2661ef7ca234099e76e3f
Ken Eguchi <no.eggchicken@gmail.com>
2019-08-24 19:38:17 +0900

    Add background

---
+++ src/components/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from "./page.module.css"
+
+export default ({children, onClick, header}) => {
+  const renderHeader = () => (
+    <div className={styles.pageHeader}>{header}</div>
+  )
+
+  return (
+    <div className={styles.pageBackground} onClick={onClick}>
+      <div className={styles.pageBackgroundShadow}>
+        {header && renderHeader()}
+        {children}
+      </div>
+    </div>
+  )
+}
