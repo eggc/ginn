8cfba8e03e1d8914003eccdc932632f72562fdf3
Ken Eguchi <no.eggchicken@gmail.com>
2019-08-29 18:37:47 +0900

    セットアップページから次に進めるようにした

--- src/components/quest/setup_page.js
+++ src/components/quest/setup_page.js
@@ -14,7 +14,7 @@ import Button from '@material-ui/core/Button'
 
 
 
-export default () => {
+export default ({onNext}) => {
   let active = {
     location: locations[0],
     characters: [],
@@ -26,17 +26,17 @@ export default () => {
       <Grid container>
         <Grid item xs={12}>
           <Typography variant="subtitle1" color="secondary">何処へ向かうか？</Typography>
-          <LocationButtons locations={locations} onChange={ (id) => active.location = locations[id] } />
+          <LocationButtons locations={locations} onChange={(id) => active.location = locations[id]} />
 
           <Typography variant="subtitle1" color="secondary">誰が向かうか？</Typography>
-          <CharacterButtons characters={characters} onChange={ (flags) => active.characters = characters.filter((c)=>flags[c.id]) } />
+          <CharacterButtons characters={characters} onChange={(flags) => active.characters = characters.filter((c)=>flags[c.id])} />
 
           <Typography variant="subtitle1" color="secondary">イベントカードを使用するか？</Typography>
-          <SpiceButtons spices={spices} onChange={ (id) => active.spice = spices[id] } />
+          <SpiceButtons spices={spices} onChange={(id) => active.spice = spices[id]} />
         </Grid>
 
         <Grid item xs={6}>
-          <Button fullWidth onClick={()=>console.log(active)}>クエストを始める</Button>
+          <Button fullWidth onClick={() => onNext(active)}>クエストを始める</Button>
         </Grid>
         <Grid item xs={6}>
           <Button fullWidth>やめる</Button>
