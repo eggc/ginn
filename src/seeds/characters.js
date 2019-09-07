6da2b8303ca06b32f072932fa955e687aa80c3be
Ken Eguchi <no.eggchicken@gmail.com>
2019-09-08 02:24:25 +0900

    ID は自動採番にする

--- src/seeds/characters.js
+++ src/seeds/characters.js
@@ -1,36 +1,35 @@
-export default [{
-  id: 0,
+const characters = [{
   title: "逆巻く炎",
   name: "ミティ",
   ability: "楔形文字の読解",
   description: "",
   power: 10
 }, {
-  id: 1,
   title: "不倒の戦士",
   name: "アレン",
   ability: "鉄拳",
   description: "",
   power: 10
 },{
-  id: 2,
   title: "月と波の巫女",
   name: "オリビエ",
   ability: "オラクル",
   description: "",
   power: 10
 },{
-  id: 3,
   title: "揺蕩うもの",
   name: "ソルブ",
   ability: "詩吟",
   description: "",
   power: 10
 },{
-  id: 4,
   title: "重戦車",
   name: "マルテンサイト",
   ability: "鉄壁",
   description: "",
   power: 12
 }]
+
+characters.forEach((c,i) => c.id = i)
+
+export default characters
--- src/seeds/locations.js
+++ src/seeds/locations.js
@@ -1,16 +1,18 @@
-export default [{
-  id: 0,
+const locations = [{
   name: '絶望の断崖',
   path: 'despair_cliff',
   description: 'かつては絶景を拝める名所として知られていたが、魔物が住み着いてから多くの人が命を奪われている。それが絶望の所以である。'
 },{
-  id: 1,
   name: '追憶の墓標',
   path: 'retrospective_grave',
   description: '無縁仏が埋葬されている墓地がある。得体の知れないものが出没すると噂が立ってから、顧みる人の姿も消えた。'
 },{
-  id: 2,
   name: '渇望の死火山',
   path: 'dead_volcano',
   description: '頻発する噴火活動のため多くの死者を出した火山が、数年前にようやく活動を終えた。しかし、そこに眠る屍が生あるものを拒み続けている。'
 }]
+
+
+locations.forEach((l,i) => l.id = i)
+
+export default locations
