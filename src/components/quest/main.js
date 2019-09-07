9f5378fb5d4f8fd9e970e3977804f3c191376ff0
Ken Eguchi <no.eggchicken@gmail.com>
2019-09-07 23:05:14 +0900

    メインページでテストデータを生成しない

--- src/components/quest/main.js
+++ src/components/quest/main.js
@@ -3,25 +3,9 @@ import { HashRouter as Router, Route, Link, Switch } from "react-router-dom";
 import SetupPage from './setup_page'
 import ProgressPage from './progress_page'
 import EvaluatePage from './evaluate_page'
-
 import Typography from '@material-ui/core/Typography'
-import locations from '../../store/locations'
-import characters from '../../store/characters'
-import spices from '../../store/spices'
-
-import SectionFactory from '../../factories/sections_factory'
-
-const testQuest = {
-  location: locations[1],
-  characters: characters,
-  spice: spices[0]
-}
-const testSections = SectionFactory.create(testQuest)
 
 export default () => {
-  const [quest, setQuest] = React.useState(testQuest)
-  const [result, setResult] = React.useState(null)
-
   return (
     <React.Fragment>
       <Router>
