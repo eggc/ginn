f7135de21eb530638ee18b4b7c59eed92ad1a8d6
Ken Eguchi <no.eggchicken@gmail.com>
2019-09-08 01:23:23 +0900

    Refactoring

--- src/factories/sections_factory.js
+++ src/factories/sections_factory.js
@@ -1,33 +1,56 @@
 import {questEvents} from '../seeds'
 
-export default {
-  create: (quest) => {
+class SectionFactory {
+  create(quest) {
+    this.quest = quest
+
     const sections = []
-    sections.push({
-      title: "開始",
-      body: quest.location.name + "の探検",
-      color: "secondary"
-    })
+    sections.push(this._createFirstSection(quest))
 
     questEvents.forEach((questEvent) => {
-      questEvent.color = "inherit"
-
-      const result = questEvent.results[Math.floor(questEvent.results.length * Math.random())]
-      const pickedCharacter = quest.characters[Math.floor(quest.characters.length * Math.random())]
-      result.body = result.body.replace(/"X"/g, pickedCharacter.name)
-      result.color = "primary"
-      sections.push(questEvent)
-      sections.push(result)
+      sections.push(this._createEventSection(questEvent))
+      sections.push(this._createResultSection(questEvent))
     })
 
-    sections.push({
-      title: "終了",
-      body: quest.location.name + "の冒険を終えた。",
-      color: "secondary"
-    })
-
+    sections.push(this._createLastSection(quest))
     sections.forEach((section, i) => section.id = i)
-
     return sections
   }
+
+  _createFirstSection() {
+    return {
+      title: "開始",
+      body: this.quest.location.name + "の探検",
+      color: "secondary"
+    }
+  }
+
+  _createEventSection(questEvent) {
+    return {
+      title: questEvent.title,
+      body: questEvent.body,
+      color: "inherit"
+    }
+  }
+
+  _createResultSection(questEvent) {
+    const result = questEvent.results[Math.floor(questEvent.results.length * Math.random())]
+    const pickedCharacter = this.quest.characters[Math.floor(this.quest.characters.length * Math.random())]
+
+    return {
+      title: result.title,
+      body: result.body.replace(/"X"/g, pickedCharacter.name),
+      color: "primary"
+    }
+  }
+
+  _createLastSection() {
+    return {
+      title: "終了",
+      body: this.quest.location.name + "の冒険を終えた。",
+      color: "secondary"
+    }
+  }
 }
+
+export default new SectionFactory()
