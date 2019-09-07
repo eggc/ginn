860d85189a0745f3927145c28b14cfc20bbca640
Ken Eguchi <no.eggchicken@gmail.com>
2019-09-07 23:22:36 +0900

    色管理は factory で行う

--- src/components/quest/sections.js
+++ src/components/quest/sections.js
@@ -4,7 +4,7 @@ import Section from './section';
 export default ({sections, step}) => {
   const renderSection = (section, i) => {
     const done = step > i
-    const color = section.results ? "inherit" : "primary"
+    const color = section.color
     const content = (
       <Section key={i} body={section.body} done={done} color={color}/>
     )
--- src/factories/sections_factory.js
+++ src/factories/sections_factory.js
@@ -5,20 +5,25 @@ export default {
     const sections = []
     sections.push({
       title: "開始",
-      body: quest.location.name + "の探検"
+      body: quest.location.name + "の探検",
+      color: "secondary"
     })
 
     questEvents.forEach((questEvent) => {
+      questEvent.color = "inherit"
+
       const result = questEvent.results[Math.floor(questEvent.results.length * Math.random())]
       const pickedCharacter = quest.characters[Math.floor(quest.characters.length * Math.random())]
       result.body = result.body.replace(/"X"/g, pickedCharacter.name)
+      result.color = "primary"
       sections.push(questEvent)
       sections.push(result)
     })
 
     sections.push({
       title: "終了",
-      body: quest.location.name + "の冒険を終えた。"
+      body: quest.location.name + "の冒険を終えた。",
+      color: "secondary"
     })
 
     sections.forEach((section, i) => section.id = i)
