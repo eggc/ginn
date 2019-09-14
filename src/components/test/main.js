edf83c51a4d4fe57da02286f5981b8b6b1d8cc55
Ken Eguchi <no.eggchicken@gmail.com>
2019-09-15 03:01:25 +0900

    テストページから Factories を削除

--- src/components/test/main.js
+++ src/components/test/main.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { HashRouter as Router, Route, Switch } from "react-router-dom"
 import Nav from '../nav'
 import Page from '../page'
-import Factories from './factories'
 import SeedTable from './seed_table'
 
 import LocationFactory from "../../factories/location_factory"
@@ -14,7 +13,6 @@ export default () => {
     <Router>
       <Nav resourceName="test" paths={["factories", "locations", "quest_event_results", "quest_events"]}></Nav>
       <Switch>
-        <Route path="/test/factories"><Factories /></Route>
         <Route path="/test/locations"><Page fullWidth><SeedTable factory={LocationFactory} /></Page></Route>
         <Route path="/test/quest_event_results"><Page fullWidth><SeedTable factory={QuestEventResultFactory} /></Page></Route>
         <Route path="/test/quest_events"><Page fullWidth><SeedTable factory={QuestEventFactory} /></Page></Route>
