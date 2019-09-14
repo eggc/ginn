227c2ad7df57bdadf54f8765b2ea7514e85a80ab
Ken Eguchi <no.eggchicken@gmail.com>
2019-09-14 17:04:15 +0900

    次へボタンを追加

--- src/components/shared/sections_page.js
+++ src/components/shared/sections_page.js
@@ -2,15 +2,20 @@ import React from 'react'
 import Page from '../page'
 import Sections from './sections'
 import Grid from '@material-ui/core/Grid'
+import { Link } from "react-router-dom"
+import Button from '@material-ui/core/Button'
 
 export default ({onComplete, sections}) => {
   const [step, setStep] = React.useState(0)
-  const onClick = () => {
-    setStep(step+1)
-    if(step >= sections.length) {
-      onComplete()
-    }
-  }
+  const onClick = () => { setStep(step+1) }
+
+  const nextButton = (
+    <Grid item xs={12}>
+      <Link onClick={onComplete}>
+        <Button fullWidth>次へ</Button>
+      </Link>
+    </Grid>
+  )
 
   return (
     <Page onClick={onClick}>
@@ -18,6 +23,7 @@ export default ({onComplete, sections}) => {
         <Grid item xs={12}>
           <Sections sections={sections} step={step} />
         </Grid>
+        {(step >= sections.length) && nextButton}
       </Grid>
     </Page>
   )
