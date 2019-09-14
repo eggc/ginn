66e7bbb00955e73fc8b2cf4c6079e555fb26661e
Ken Eguchi <no.eggchicken@gmail.com>
2019-09-14 15:41:49 +0900

    Move sections to shared

--- src/components/quest/progress_page.js
+++ src/components/quest/progress_page.js
@@ -1,24 +1,10 @@
 import React from 'react'
-import Page from '../page'
-import Sections from './sections'
-import Grid from '@material-ui/core/Grid'
+import SectionsPage from '../shared/sections_page'
 
 export default ({history, sections}) => {
-  const [step, setStep] = React.useState(0)
-  const onClick = () => {
-    setStep(step+1)
-    if(step >= sections.length) {
-      history.push("/quest/evaluate")
-    }
+  const onComplete = () => {
+    history.push("/quest/evaluate")
   }
 
-  return (
-    <Page onClick={onClick}>
-      <Grid container>
-        <Grid item xs={12}>
-          <Sections sections={sections} step={step} />
-        </Grid>
-      </Grid>
-    </Page>
-  )
+  return <SectionsPage sections={sections} onComplete={onComplete} />
 }
---
+++ src/components/shared/sections_page.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Page from '../page'
+import Sections from './sections'
+import Grid from '@material-ui/core/Grid'
+
+export default ({onComplete, sections}) => {
+  const [step, setStep] = React.useState(0)
+  const onClick = () => {
+    setStep(step+1)
+    if(step >= sections.length) {
+      onComplete()
+    }
+  }
+
+  return (
+    <Page onClick={onClick}>
+      <Grid container>
+        <Grid item xs={12}>
+          <Sections sections={sections} step={step} />
+        </Grid>
+      </Grid>
+    </Page>
+  )
+}
