7a16746890b6189c4621d16ae196a33d2cba4c5a
Ken Eguchi <no.eggchicken@gmail.com>
2019-10-20 22:45:17 +0900

    Updates

---
+++ precache-manifest.84b2c6e356beb0a2d1cd198e0735f6bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09fa021ac3ee28e10274f68b1ff2e4a6",
+    "url": "/ginn/index.html"
+  },
+  {
+    "revision": "dad57755dd5fe4d3939d",
+    "url": "/ginn/static/css/main.8d3de7ee.chunk.css"
+  },
+  {
+    "revision": "93b42f36747a0a4177b4",
+    "url": "/ginn/static/js/2.12cd6590.chunk.js"
+  },
+  {
+    "revision": "dad57755dd5fe4d3939d",
+    "url": "/ginn/static/js/main.dc266a5f.chunk.js"
+  },
+  {
+    "revision": "3a65ecdcc74945ab9666",
+    "url": "/ginn/static/js/runtime~main.80879b9c.js"
+  }
+]);
